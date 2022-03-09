255dc31a3c0244f36d0bc833138883cb428fcbd4
Aleksandr Melnikov <aleksandr.melnikov@giant.ai>
2022-03-09 07:46:56 +0300

    temperature sensor added

--- src/main.ino
+++ src/main.ino
@@ -1,19 +1,41 @@
 // https://www.makerguides.com/a4988-stepper-motor-driver-arduino-tutorial/
 #include <AccelStepper.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
 
+// stepper pins
 #define dirPin 2
 #define stepPin 3
 #define motorInterfaceType 1
 
-const int BUTTON_CLOSE_PIN = 4;
-const int BUTTON_OPEN_PIN = 5;
+// temperature sensor pin
+#define ONE_WIRE_BUS 7
+
+// button pins
+#define BUTTON_CLOSE_PIN 4
+#define BUTTON_OPEN_PIN 5
+
+// temperature limits
+#define MIN_T 17
+#define MAX_T 25
+
+// Here will be stepper max position after the interval calibration
 int MAX_X = 0;
 
-// Create a new instance of the AccelStepper class:
+// setup temperature sensor
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature t_sensor(&oneWire);
+
+// setup stepper
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+
 void setup() {
   Serial.begin(9600);
 
+  // init temperature sensor
+  t_sensor.begin();
+
+  // setup pin modes for the buttons
   pinMode(BUTTON_CLOSE_PIN, INPUT_PULLUP);
   pinMode(BUTTON_OPEN_PIN, INPUT_PULLUP);
 
@@ -44,11 +66,21 @@ void setup() {
   }
 
 }
-void loop() { 
-  // TODO
-  // call temperature sensor, calculate the new position by the formula
-  int x = int(MAX_X / 2.0);
+void loop() {
+  // Send the command to get temperature readings 
+  t_sensor.requestTemperatures();
+  
+  delay(1000);
+  
+  // read temperature
+  float t = t_sensor.getTempCByIndex(0);
+
+  // calculate position by linear interpolation
+  int x = int((t - MIN_T) / (MAX_T - MIN_T) * MAX_X);
+
   Serial.print("go to x: ");
   Serial.println(x);
+
+  // actuate
   stepper.runToNewPosition(x);
 }
