a3d163cb594331c183fca9a19dc559fe3bb2bc02
Aleksandr Melnikov <aleksandr.melnikov@giant.ai>
2022-03-10 18:41:46 +0300

    minor changes

--- src/main.ino
+++ src/main.ino
@@ -4,8 +4,8 @@
 #include <DallasTemperature.h>
 
 // stepper pins
-#define dirPin 2
-#define stepPin 3
+#define DIR_PIN 2
+#define STEP_PIN 3
 #define motorInterfaceType 1
 
 // temperature sensor pin
@@ -30,7 +30,7 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature t_sensor(&oneWire);
 
 // setup stepper
-AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+AccelStepper stepper = AccelStepper(motorInterfaceType, STEP_PIN, DIR_PIN);
 
 // perform a calibration of the operational range
 void setup() {
@@ -44,7 +44,7 @@ void setup() {
   pinMode(BUTTON_OPEN_PIN, INPUT_PULLUP);
 
   // Set the maximum speed in steps per second:
-  stepper.setMaxSpeed(1000);
+  stepper.setMaxSpeed(SPEED);
   stepper.setSpeed(SPEED);
   // go backward and find zero position
   while (1) {
@@ -64,6 +64,8 @@ void setup() {
     if (is_button_pressed == LOW) {
       Serial.println("Open button pressed");
       MAX_X = stepper.currentPosition();
+      Serial.print("Max position: ");
+      Serial.println(MAX_X);
       break;
     }
     stepper.runSpeed();
@@ -76,30 +78,22 @@ void loop() {
   while (t > 84 || t < -100) {
     t_sensor.requestTemperatures();
     delay(1000);
-    t = t_sensor.getTempCByIndex(0);
+    t = round(t_sensor.getTempCByIndex(0));
     delay(1000);
     // read temperature
-    Serial.print("got t: ");
+    Serial.print("temperature: ");
     Serial.println(t);
   }
   
-  
+  // limit the temperature
+  t = constrain(t, MIN_T, MAX_T);
 
   // calculate position by the linear interpolation
-  if (t < MIN_T) {
-    t = MIN_T;
-  }
-  if (t > MAX_T) {
-    t = MAX_T;
-  }
-
   int x = int((t - MIN_T) / (MAX_T - MIN_T) * MAX_X);
-  
-  Serial.print("Max position: ");
-  Serial.println(MAX_X);
 
-  Serial.print("go to x: ");
-  Serial.println(x);  
+  // debug print
+  // Serial.print("go to position: ");
+  // Serial.println(x);  
 
   // actuate
   stepper.moveTo(x);
